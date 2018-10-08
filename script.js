a687b5d7b00afb6a1f98848ad48668ee15ccbac9
InshalFatima <38943108+InshalFatima@users.noreply.github.com>
2018-10-08 17:49:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// write your JS code here
+
+// function initialize(){
+//     console.log(input);
+// }
+// let output="";
+// function display(y){
+//     console.log(y);
+//     output+=y;
+//     console.log(y);
+//     document.querySelector(".card-header").innerHTML=output;
+// }
